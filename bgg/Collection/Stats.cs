3e5b36627a1676670a453bbbdef995ea6c9713d5
Tyson J. Hayes <artwdrahn@gmail.com>
2014-02-24 12:11:42 -0800

    Make it so

---
+++ bgg/Collection/Stats.cs
@@ -0,0 +1,35 @@
+﻿namespace BGGAPI
+{
+    using BGGAPI.Shared;
+
+    /// <summary>
+    /// The stats of a particular game.
+    /// </summary>
+    public class Stats
+    {
+        /// <summary>
+        /// Gets or sets the min players.
+        /// </summary>
+        public int MinPlayers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max players.
+        /// </summary>
+        public int MaxPlayers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the playing time.
+        /// </summary>
+        public int PlayingTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the num owned.
+        /// </summary>
+        public int NumOwned { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rating.
+        /// </summary>
+        public Ratings Rating { get; set; }
+    }
+}
