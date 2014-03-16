0ded8d043eb21912aaa7df66d7ddff8e077cb2ce
Tyson J. Hayes <artwdrahn@gmail.com>
2014-03-16 10:44:17 -0700

    StyleCoppin'

--- BGGAPI_UnitTests/Integration/FamilyReturn.cs
+++ BGGAPI_UnitTests/Integration/FamilyReturn.cs
@@ -1,4 +1,16 @@
-﻿namespace BGGAPI_UnitTests.Integration
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FamilyReturn.cs" company="Tyson J. Hayes">
+//   © 2014 - Refer to the License.md for the project.
+// </copyright>
+// <summary>
+//   Integration for the Family API calls.
+//   See <see cref="BGGAPI.Family.Request" /> for the Request calls
+//   See <see cref="BGGAPI.Family.Return" /> for the return response.
+//   See <see cref="BGGAPI.Family.Item" /> for the individual items returned.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BGGAPI_UnitTests.Integration
 {
     using System.Collections.Generic;
     using System.Linq;
@@ -13,7 +25,10 @@
     using Return = BGGAPI.Family.Return;
 
     /// <summary>
-    /// Summary description for UnitTest1
+    /// Integration for the Family API calls.
+    /// See <see cref="BGGAPI.Family.Request"/> for the Request calls
+    /// See <see cref="BGGAPI.Family.Return"/> for the return response.
+    /// See <see cref="BGGAPI.Family.Item"/> for the individual items returned.
     /// </summary>
     [TestClass]
     public class FamilyReturn
@@ -31,10 +46,19 @@
         /// </summary>
         public TestContext TestContext { get; set; }
 
+        /// <summary>
+        /// Gets or sets the returned object.
+        /// </summary>
         private static Return Return { get; set; }
 
+        /// <summary>
+        /// Gets or sets the returned items.
+        /// </summary>
         private static List<int> ReturnedItems { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name count.
+        /// </summary>
         private static int NameCount { get; set; }
 
             /// <summary>
@@ -53,12 +77,18 @@
             ReturnedItems = new List<int>();
         }
 
+        /// <summary>
+        /// The integration family returns more than zero.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsMoreThanZero()
         {
             Assert.IsTrue(Return.Items.Count > 0);
         }
 
+        /// <summary>
+        /// The integration family returns id match.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsIDMatch()
         {
@@ -70,6 +100,9 @@
             CollectionAssert.AreEqual(ReturnedItems, RequestID);
         }
 
+        /// <summary>
+        /// The integration family returns name not null.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsNameNotNull()
         {
@@ -81,34 +114,41 @@
             Assert.AreEqual(NameCount, Return.Items[0].Names.Count);
         }
 
+        /// <summary>
+        /// The integration family returns thumbnail not null.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsThumbnailNotNull()
         {
             Assert.IsNotNull(Return.Items[0].Thumbnail);
         }
 
+        /// <summary>
+        /// The integration family returns image not null.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsImageNotNull()
         {
             Assert.IsNotNull(Return.Items[0].Image);
         }
 
+        /// <summary>
+        /// The integration family returns description is not null.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsDescriptionIsNotNull()
         {
             Assert.IsNotNull(Return.Items[0].Description);
         }
 
+        /// <summary>
+        /// The integration family returns links.
+        /// </summary>
         [TestMethod]
         public void IntegrationFamilyReturnsLinks()
         {
             var count = Return.Items[0].Links.Count(link => !string.IsNullOrWhiteSpace(link.value));
             Assert.AreEqual(count, Return.Items[0].Links.Count);
-        }
-
-        private static bool GetLinks(Item item)
-        {
-            return item.Links != null;
         }
     }
 }
