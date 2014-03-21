ff7f24f116387ea5b7a8125ce90c402192dcd7ed
Tyson J. Hayes <artwdrahn@gmail.com>
2014-03-21 15:51:19 -0700

    File Cleanup and Comments

--- BGGAPI/Forums/ForumList/Request.cs
+++ BGGAPI/Forums/ForumList/Request.cs
@@ -1,15 +1,38 @@
-﻿namespace BGGAPI.ForumList
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Request.cs" company="Tyson J. Hayes">
+//   © 2014 - Refer to the License.md for the project.
+// </copyright>
+// <summary>
+//  The Request object for the ForumList.
+//  <see cref="Request"/> for the request object.
+//  <see cref="Return"/> for the return object.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BGGAPI.ForumList
 {
+    /// <summary>
+    /// The request object for the forum list.
+    /// </summary>
     public class Request
     {
+        /// <summary>
+        /// The family type.
+        /// </summary>
         public enum FamilyType
         {
             thing,
             family
         }
 
+        /// <summary>
+        /// Gets or sets the id for the forum list.
+        /// </summary>
         public int ID { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type being requested.
+        /// </summary>
         public FamilyType Type { get; set; }
     }
 }
