7514d53248dadd43d3d300fb0d4536d504caa623
Tyson J. Hayes <artwdrahn@gmail.com>
2014-03-25 08:11:56 -0700

    Adding Threads Request/Return

---
+++ BGGAPI/Forums/Threads/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BGGAPI.Threads
+{
+    public class Request
+    {
+        public int ID { get; set; }
+
+        public int MinArticleID { get; set; }
+
+        // minarticledate=YYYY-MM-DD
+        // minarticledate=YYYY-MM-DD%20HH%3AMM%3ASS
+        public DateTime MinArticleDate { get; set; }
+
+        // Limits the number of articles returned to no more than NNN.
+        public int Count { get; set; }
+
+        // Not supported yet.
+        // public string UserName { get; set; }
+    }
+}
---
+++ BGGAPI/Forums/Threads/Return.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BGGAPI.Threads
+{
+    public class Return : Shared.IReturn
+    {
+        public int TotalItems { get; set; }
+        public string TermsOfUse { get; set; }
+
+        public int ID { get; set; }
+
+        public int NumArticles { get; set; }
+
+        public Uri Link { get; set; }
+
+        public string Subject { get; set; }
+
+        public List<Article> Articles { get; set; }
+    }
+
+    public class Article
+    {
+        public int ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public Uri Link { get; set; }
+
+        public DateTime PostDate { get; set; }
+    }
+}
