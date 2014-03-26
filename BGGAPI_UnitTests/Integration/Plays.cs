33331a02946cc952543bd4729fb660acb5a32d32
Tyson J. Hayes <artwdrahn@gmail.com>
2014-03-26 07:36:32 -0700

    adding test files

---
+++ BGGAPI_UnitTests/Integration/Plays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BGGAPI_UnitTests.Integration
+{
+    [TestClass]
+    public class Plays
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
